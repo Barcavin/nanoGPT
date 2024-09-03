09736c855f8d653ca6fe9e281499c83f9c6b2965
barcavin <kevindong1994@gmail.com>
2024-09-03 03:33:29 -0400

    no proj v

--- model.py
+++ model.py
@@ -32,7 +32,7 @@ class CausalSelfAttention(nn.Module):
         super().__init__()
         assert config.n_embd % config.n_head == 0
         # key, query, value projections for all heads, but in a batch
-        self.c_attn = nn.Linear(config.n_embd, 3 * config.n_embd, bias=config.bias)
+        self.c_attn = nn.Linear(config.n_embd, 2 * config.n_embd, bias=config.bias)
         
         # Set the layer as non-trainable
         for param in list(self.c_attn.parameters()):
@@ -55,7 +55,8 @@ class CausalSelfAttention(nn.Module):
         B, T, C = x.size() # batch size, sequence length, embedding dimensionality (n_embd)
 
         # calculate query, key, values for all heads in batch and move head forward to be the batch dim
-        q, k, v  = self.c_attn(x).split(self.n_embd, dim=2)
+        q, k  = self.c_attn(x).split(self.n_embd, dim=2)
+        v = x
         k = k.view(B, T, self.n_head, C // self.n_head).transpose(1, 2) # (B, nh, T, hs)
         q = q.view(B, T, self.n_head, C // self.n_head).transpose(1, 2) # (B, nh, T, hs)
         v = v.view(B, T, self.n_head, C // self.n_head).transpose(1, 2) # (B, nh, T, hs)
